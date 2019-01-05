723bd40231f6e6446a5a9de2fc684a8a78b5afbb
r4jsg <therajsagar@gmail.com>
2019-01-05 22:53:56 +0530

    Updates

---
+++ src/reducers/tableData.js
@@ -0,0 +1,42 @@
+import {
+  ADD_TABLE_ITEM,
+  DELETE_TABLE_ITEM,
+  TOGGLE_TABLE
+} from "../constants/constants.js";
+
+var initialTableData = [];
+
+for (let i = 0; i < 16; i++) {
+  initialTableData.push([]);
+}
+
+const tableData = (state = initialTableData, action) => {
+  switch (action.type) {
+    case ADD_TABLE_ITEM:
+      var stateCopy = [];
+      for (let i = 0; i < 16; i++) {
+        stateCopy.push(state[i].slice());
+      }
+      stateCopy[action.tableId].push(action.item);
+      return stateCopy;
+    case DELETE_TABLE_ITEM:
+       stateCopy = [];
+      for (let i = 0; i < 16; i++) {
+        stateCopy.push(state[i].slice());
+      }
+      console.log(action);
+      stateCopy[action.tableId].splice(action.id, 1);
+      return stateCopy;
+    case TOGGLE_TABLE:
+       stateCopy = [];
+      for (let i = 0; i < 16; i++) {
+        stateCopy.push(state[i].slice());
+      }
+      stateCopy[action.id] = [];
+      return stateCopy;
+    default:
+      return state;
+  }
+};
+
+export default tableData;
