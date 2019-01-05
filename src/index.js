ad041dc92b73baf59d7119d13b97ca5cd4754bac
r4jsg <therajsagar@gmail.com>
2019-01-05 15:46:58 +0530

    Intro to redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
 import './index.css';
+import { createStore } from 'redux'
+
+
+//action creator
+const addItem = item => {
+  return {
+    type: "ADD_ITEM",
+    item: item
+  };
+};
+
+//reducer
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_ITEM":
+      return [...state, action.item];
+    default:
+      return state;
+  }
+};
+
+var store = createStore(reducer);
+
+console.log(store.dispatch(addItem("a")));
+console.log(store.getState());
+console.log(store.dispatch(addItem("b")));
+console.log(store.getState());
+console.log(store.dispatch(addItem("c")));
+console.log(store.getState());
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
 
 
 
-const App = () => <h1>Hello World</h1>
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
